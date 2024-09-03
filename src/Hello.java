ce2919d7cda0b79fe51e1fcff893597db6d5ad42
AKS <AKS@AKS>
2024-09-03 14:08:29 -0500

    Added my own name and major.

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Bryce Helms");
 		System.out.println("My major is Computer Engineering");
+		System.out.println("Alexander Stadler");
+		System.out.println("My major is Computer Science");
 		
 	}
 }
